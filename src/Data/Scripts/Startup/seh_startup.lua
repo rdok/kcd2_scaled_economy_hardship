08abe284b4e38e87bf74d947c4fd191931b92644
Rizart Dokollari <r.dokollari@gmail.com>
2025-03-13 22:56:27 +0000

    chore: Load mod scripts.

---
+++ src/Data/Scripts/Startup/seh_startup.lua
@@ -0,0 +1,3 @@
+Script.LoadScript("Scripts/ScaledEconomyHardship/main.lua")
+Script.LoadScript("Scripts/ScaledEconomyHardship/log.lua")
+Script.LoadScript("Scripts/ScaledEconomyHardship/perk_manager.lua")
